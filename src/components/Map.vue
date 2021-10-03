2cc1b78f26ec021b962142e3484ce81c45be4f1e
stefnotch <brandmairstefan@gmail.com>
2021-10-03 12:17:30 +0200

    Fix stuff and add stuff

---
+++ src/components/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="map">
+    {{name}}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType, ref, watch, shallowRef } from 'vue'
+
+export default defineComponent({
+  components: {},
+  setup(props, context) {
+   let name = ref("my name")
+    return {
+      name,
+    }
+  },
+})
+</script>
